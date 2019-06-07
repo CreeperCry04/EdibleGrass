6a40ae510a63b1f842536f2c5fea38f2d7dd5233
CreeperCry04 <39033698+CreeperCry04@users.noreply.github.com>
2019-06-06 23:21:59 -0400

    Updated to version 1.2

--- src/xyz/creepercry/ediblegrass/events/PlayerRightClick.java
+++ src/xyz/creepercry/ediblegrass/events/PlayerRightClick.java
@@ -4,25 +4,30 @@ import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import xyz.creepercry.ediblegrass.EdibleGrass;
 
 public class PlayerRightClick implements Listener {
 	
 	@EventHandler
-	public void onClick(PlayerInteractEvent event) {
-		final Player player = event.getPlayer();
-		if (event.getClickedBlock().getType() == Material.GRASS && event.getAction() == Action.RIGHT_CLICK_BLOCK)
-			if (player.getItemInHand() == null || player.getItemInHand().getType() == Material.AIR) {
-				event.getClickedBlock().setType(Material.DIRT);
+	public void onClick(PlayerInteractEvent e) {
+		final Player player = e.getPlayer();
+		final ItemStack item = player.getItemInHand();
+		final Block block = e.getClickedBlock();
+		if (block.getType() == Material.GRASS && e.getAction() == Action.RIGHT_CLICK_BLOCK)
+			if (item == null || item.getType() == Material.AIR) {
+				block.setType(Material.DIRT);
 				player.setSaturation(player.getSaturation() + EdibleGrass.getInstance().getConfig().getInt("food.saturation"));
 				player.setFoodLevel(player.getFoodLevel() + EdibleGrass.getInstance().getConfig().getInt("food.food-level"));
-
+				if (EdibleGrass.getInstance().getConfig().getBoolean("rewards.disabled"))
+					return;
 				final Random r = new Random();
 				final int chance = r.nextInt(100);
 				if (chance <= EdibleGrass.getInstance().getConfig().getInt("rewards.chance")) {
